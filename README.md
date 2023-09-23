62043f1313d97774a1a61f649846e1e837583b7c
Bart Louwers <bart@emeel.net>
2023-09-23 22:04:15 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# MapLibre Native Nix Flake
+
+> **Warning**
+> This is not officially supported.
+
+This directory contains files to set up a MapLibre Native development environment. [Nix](https://nixos.org/) is used which is a declarative language and package manager for reproducible builds. Note however that Nix is not used for the build itself.
+
+First [install Nix](https://nixos.org/download.html); Linux, macOS and Windows ([WSL2](https://learn.microsoft.com/en-us/windows/wsl/install) are supported. If you want to learn how Nix works, a good introduction is [Zero to Nix](https://zero-to-nix.com/).
+
+## Nix Flake
+
+A *Nix flake* is used to set up the environment, which is a description of dependencies pinned to a specific version (see `flake.nix` and `flake.lock` in this directory). Flakes are an an experimental feature of Nix which needs to be enabled in your settings or via flags passed to the commands below.  You need to add the flags `--extra-experimental-features nix-command --extra-experimental-features flakes` to the commands below or add:
+
+```
+experimental-features = nix-command flakes
+```
+
+to your `~/.config/nix/nix.conf`.
+
+See also [Nix Flakes - Enable Flakes](https://nixos.wiki/wiki/Flakes#Enable_flakes).
+
+
+## Shell
+
+To enter a shell with the CI environment, use (in this directory):
+
+```
+nix develop
+```
+
+## Docker container
+
+To create a Docker image which contains the CI environment:
+
+```
+nix build .#dockerImage
+docker load < result
+```
+
+Please create an issue if you are interested in having this container made available without having to create it yourself.
+
+## Updating
+
+All dependencies are pinned in the lock file. To update all inputs
+
+```
+nix flake update --commit-lock-file
+```
+
+Individual inputs can also be updated. See [`nix flake update`](https://nixos.org/manual/nix/stable/command-ref/new-cli/nix3-flake-update.html).
+
---
+++ flake.nix
@@ -0,0 +1,89 @@
+{
+  description = "MapLibre Native for Android CI";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    android.url = "github:tadfisher/android-nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, android }:
+    {
+      overlay = final: prev: {
+        inherit (self.packages.${final.system}) android-sdk android-studio;
+      };
+    }
+    //
+    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-darwin" ] (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+          overlays = [
+            self.overlay
+          ];
+        };
+        env = let androidHome = "${android-sdk}/share/android-sdk"; in
+          {
+            JAVA_HOME = jdk.home;
+            ANDROID_HOME = androidHome;
+            ANDROID_SDK_ROOT = androidHome;
+            GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${android-sdk}/share/android-sdk/build-tools/31.0.0/aapt2 -Dorg.gradle.daemon=false";
+          };
+        jdk = pkgs.jdk;
+        android-sdk = android.sdk.${system} (sdkPkgs: with sdkPkgs; [
+          # Useful packages for building and testing.
+          build-tools-31-0-0
+          cmdline-tools-latest
+          # emulator
+          platform-tools
+          platforms-android-33
+          ndk-25-1-8937393
+
+          # Other useful packages for a development environment.
+          # sources-android-30
+          # system-images-android-30-google-apis-x86
+          # system-images-android-30-google-apis-playstore-x86
+        ]);
+        paths = with pkgs; [
+          cmake
+          ninja
+          bash
+          nodejs
+          gnumake
+          coreutils
+          gnugrep
+          gnused
+          git
+          gradle
+          google-cloud-sdk
+          ccache
+          /*   pkgs.androidStudioPackages.stable; */
+          /*   pkgs.androidStudioPackages.beta; */
+          /*   pkgs.androidStudioPackages.preview; */
+          /*   pkgs.androidStudioPackage.canary; */
+        ];
+        dockerImage = pkgs.dockerTools.buildLayeredImage {
+          name = "maplibre-gl-native-android-builder";
+          created = "now";
+          extraCommands = ''mkdir tmp
+					chmod 1777 tmp'';
+          config = {
+            Env = map (name: "${name}=${env.${name}}") (builtins.attrNames env);
+          };
+          contents = paths ++ [ pkgs.dockerTools.usrBinEnv ];
+        };
+      in
+      {
+				devShells = {
+				  default = pkgs.mkShell {
+            inherit env;
+            packages = paths;
+          };
+				};
+        packages = {
+          dockerImage = dockerImage;
+        };
+      }
+    );
+}
